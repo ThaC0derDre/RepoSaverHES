834c48c374d3a5e4cb83321311cf98f1b47d0b4c
AndresGutierrez <c0dingw.dre@gmail.com>
2022-11-16 13:35:36 -0800

    Initial Commit

---
+++ RepoSaverHES/RepoSaverHESApp.swift
@@ -0,0 +1,17 @@
+//
+//  RepoSaverHESApp.swift
+//  RepoSaverHES
+//
+//  Created by Andres Gutierrez on 11/16/22.
+//
+
+import SwiftUI
+
+@main
+struct RepoSaverHESApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
